39d8bbee5c24409bf7a3acdaf6bb735637927af1
testsbs <testsbs@gmail.com>
2023-05-21 17:28:09 +0900

    좀비플래닛 기본 작업용

---
+++ PVZ/Assets/Scripts/BGSprite.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGSprite : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        Debug.Log($"Å¬¸¯ Àû¿ëµÊ : {this.name} ");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PVZ/Assets/Scripts/InGameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameManager : MonoBehaviour
+{
+
+    public int WSize = 8;
+    public int HSize = 5;
+
+    public Vector2 BlockSize = new Vector2(1.3f, 1.3f);
+    public BGSprite m_CloneBGSprite = null;
+
+    public BGSprite[,] m_AllBGSpitre = null;
+
+    void CreateBGClickSprite()
+    {
+        m_AllBGSpitre = new BGSprite[HSize, WSize];
+        for (int y = 0; y < HSize; y++)
+        {
+            for (int x = 0; x < WSize; x++)
+            {
+                BGSprite cloneSprite = GameObject.Instantiate(m_CloneBGSprite, this.transform);
+                cloneSprite.transform.localPosition = new Vector3(BlockSize.x * x, BlockSize.y * y, 0f);
+                cloneSprite.name = $"CloneBG_[{x},{y}]";
+                
+                m_AllBGSpitre[y, x] = cloneSprite;
+            }
+        }
+
+    }
+
+    void Start()
+    {
+        CreateBGClickSprite();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
